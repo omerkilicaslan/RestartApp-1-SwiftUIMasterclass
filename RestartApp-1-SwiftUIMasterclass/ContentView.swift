82cd0761086359c9067566e188f9838815dcaeec
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-11-26 19:53:33 +0300

    Initial Commit

---
+++ RestartApp-1-SwiftUIMasterclass/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RestartApp-1-SwiftUIMasterclass
+//
+//  Created by Ömer Faruk Kılıçaslan on 26.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RestartApp-1-SwiftUIMasterclass/RestartApp_1_SwiftUIMasterclassApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp_1_SwiftUIMasterclassApp.swift
+//  RestartApp-1-SwiftUIMasterclass
+//
+//  Created by Ömer Faruk Kılıçaslan on 26.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp_1_SwiftUIMasterclassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
