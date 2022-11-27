9ed15da6be9c9272bd31c81d9edce968f3980e06
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-11-27 22:04:40 +0300

    Reusable components figured.

--- RestartApp-1-SwiftUIMasterclass/Screens/OnBoardingView.swift
+++ RestartApp-1-SwiftUIMasterclass/Screens/OnBoardingView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct OnBoardingView: View {
+    //MARK: - Property
     
     @AppStorage("onboarding") var isOnboardingViewActive:Bool = true
+    
+    //MARK: - Body
+    
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0
     
     var body: some View {
         ZStack{
@@ -76,7 +82,7 @@ struct OnBoardingView: View {
                         
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
                         
                         Spacer()
                     }
@@ -97,15 +103,29 @@ struct OnBoardingView: View {
                         }
                         .foregroundColor(.white)
                         .frame(width: 80, height: 80, alignment: .center)
-                        .onTapGesture {
-                            isOnboardingViewActive = false
-                        }
+                        .offset(x: buttonOffset)
+                        .gesture(
+                        DragGesture()
+                            .onChanged({ gesture in
+                                if gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80 {
+                                    buttonOffset = gesture.translation.width
+                                }
+                            })
+                            .onEnded({ _ in
+                                
+                                if buttonOffset > buttonWidth / 1.5 {
+                                    buttonOffset = buttonWidth - 80
+                                    isOnboardingViewActive = false
+                                }
+                                buttonOffset = 0
+                            })
+                        )
                         
                         Spacer()
                     } //: HSTACK
                     
                 } //: FOOTER
-                .frame(height: 80, alignment: .center)
+                .frame(width:buttonWidth ,height: 80, alignment: .center)
                 .padding()
             } //: VSTACK
         } //: ZSTACK
